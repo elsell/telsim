adf0671786db6bc1ee45d9d3c50bcb3bb6b44f49
elsell <john.sell@mail.wlc.edu>
2019-08-28 16:01:01 -0500

    Revised error message

--- renderer/js/tello_translate.js
+++ renderer/js/tello_translate.js
@@ -1,95 +1,96 @@
 class TelloTranslate
 {
     constructor()
+    {}
+    
+    static commandMap = 
     {
-        self.commandMap = 
-        {
-            "land":{  
-                "function":c_Land,
-                "argc":0,
-                "desc":"Auto landing."
-             },
-             "emergency":{  
-                "function":c_Emergency,
-                "argc":0,
-                "desc":"Stop motors immediately."
-             },
-             "up":{  
-                "function":c_Up,
-                "argc":1,
-                "desc":"Ascend to 'x' cm. x = [20,500]"
-             },
-             "down":{  
-                "function":c_Down,
-                "argc":1,
-                "desc":"Descend to 'x' cm. x = [20,500]"
-             },
-             "left":{  
-                "function":c_Left,
-                "argc":1,
-                "desc":"Fly left for 'x' cm. x = [20,500]"
-             },
-             "right":{  
-                "function":c_Right,
-                "argc":1,
-                "desc":"Fly right for 'x' cm. x = [20,500]"
-             },
-             "forward":{  
-                "function":c_Forward,
-                "argc":1,
-                "desc":"Fly forward for 'x' cm. x = [20,500]"
-             },
-             "back":{  
-                "function":c_Back,
-                "argc":1,
-                "desc":"Fly backward for 'x' cm. x = [20,500]"
-             },
-             "cw":{  
-                "function":c_Cw,
-                "argc":1,
-                "desc":"Rotate 'x' degrees clockwise. x = [1,360]"
-             },
-             "ccw":{  
-                "function":c_Ccw,
-                "argc":1,
-                "desc":"Rotate 'x' degrees counterclockwise. x = [1,360]"
-             },
-             "flip":{  
-                "function":c_Flip,
-                "argc":1,
-                "desc":"Flip in 'x' direction. 'l' = left 'r' = right 'f' = forward 'b' = back"
-             },
-             "go":{  
-                "function":c_Go,
-                "argc":4,
-                "desc":"Fly to 'x' 'y' 'z' at 'speed' (cm/s). " +
-                "'x' = [-500,500] 'y' = [-500,500] 'z' = [-500,500] " + 
-                "'speed' = [10,100]"
-             },
-             "stop":{  
-                "function":c_Stop,
-                "argc":0,
-                "desc":"Hovers in the air. Note: works at any time."
-             },
-             "curve":{  
-                "function":c_Curve,
-                "argc":7,
-                "desc":"Fly at a curve according to the two given coordinates."
-             },
-             "speed":{  
-                "function":c_Speed,
-                "argc":1,
-                "desc":"Set speed to 'x' cm/s. x = [10,100]"
-             },
-             
-        };
-    }
+        "land":{  
+            "function":c_Land,
+            "argc":0,
+            "desc":"Auto landing."
+            },
+            "emergency":{  
+            "function":c_Emergency,
+            "argc":0,
+            "desc":"Stop motors immediately."
+            },
+            "up":{  
+            "function":c_Up,
+            "argc":1,
+            "desc":"Ascend to 'x' cm. x = [20,500]"
+            },
+            "down":{  
+            "function":c_Down,
+            "argc":1,
+            "desc":"Descend to 'x' cm. x = [20,500]"
+            },
+            "left":{  
+            "function":c_Left,
+            "argc":1,
+            "desc":"Fly left for 'x' cm. x = [20,500]"
+            },
+            "right":{  
+            "function":c_Right,
+            "argc":1,
+            "desc":"Fly right for 'x' cm. x = [20,500]"
+            },
+            "forward":{  
+            "function":c_Forward,
+            "argc":1,
+            "desc":"Fly forward for 'x' cm. x = [20,500]"
+            },
+            "back":{  
+            "function":c_Back,
+            "argc":1,
+            "desc":"Fly backward for 'x' cm. x = [20,500]"
+            },
+            "cw":{  
+            "function":c_Cw,
+            "argc":1,
+            "desc":"Rotate 'x' degrees clockwise. x = [1,360]"
+            },
+            "ccw":{  
+            "function":c_Ccw,
+            "argc":1,
+            "desc":"Rotate 'x' degrees counterclockwise. x = [1,360]"
+            },
+            "flip":{  
+            "function":c_Flip,
+            "argc":1,
+            "desc":"Flip in 'x' direction. 'l' = left 'r' = right 'f' = forward 'b' = back"
+            },
+            "go":{  
+            "function":c_Go,
+            "argc":4,
+            "desc":"Fly to 'x' 'y' 'z' at 'speed' (cm/s). " +
+            "'x' = [-500,500] 'y' = [-500,500] 'z' = [-500,500] " + 
+            "'speed' = [10,100]"
+            },
+            "stop":{  
+            "function":c_Stop,
+            "argc":0,
+            "desc":"Hovers in the air. Note: works at any time."
+            },
+            "curve":{  
+            "function":c_Curve,
+            "argc":7,
+            "desc":"Fly at a curve according to the two given coordinates."
+            },
+            "speed":{  
+            "function":c_Speed,
+            "argc":1,
+            "desc":"Set speed to 'x' cm/s. x = [10,100]"
+            },
+            
+    };
+    
 
     // Given a Telsim API command, will return the corresponding Javascript 
     // function and an array of arguments to be passed to the function.
     // Ex:
     // Translate("speed 10") -> {"function": c_Speed, "argv": [10]}
-    Translate(command)
+    static Translate(command)
     {
         if(typeof command !== "string")
         {
@@ -102,7 +103,7 @@ class TelloTranslate
         
         var rootCommand = commandBuff[0];
 
-        var commandData = self.commandMap[rootCommand];
+        var commandData = TelloTranslate.commandMap[rootCommand];
 
         // We will only check if there are not enough arguments provided for 
         // the desired command. We will not check if we are given too many.
@@ -111,7 +112,8 @@ class TelloTranslate
         if(commandBuff.length < commandData.argc + 1)
         {
             throw(
-                "Invalid Command: '" + command + "' (Argument Count Mismatch)"
+                "Invalid Command: '" + command + "' (Argument Count Mismatch," +
+                " Expecting " + String(commandData.argc) + " arguments)"
             );
         }
 
