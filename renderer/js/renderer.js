eda865d2163e62c07e800f0ffe50ef2e0f105602
elsell <john.sell@mail.wlc.edu>
2019-08-30 18:57:26 -0500

    Added camera control infrastructure, basic UI elements

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -6,14 +6,14 @@ let COPTER_SCALE = .068;
 let COPTER_MODEL_SCALE = .321;
 let COPTER_SPEED = 1;
 
+let CAMERA_POS = { x:0, y: 0, z: 1000 };
+let CAMERA_FOCUS = { x:0, y: 0, z: 0 };
+
 // How many pixels represent 1 foot
 let ONE_FOOT = 20;
 
 // Length of one side of the square floor (feet)
 let FLOOR_SIZE = 11;
-
-
-
 
 let COPTER_MODEL;
 
@@ -28,7 +28,6 @@ function setup()
     createCanvas(windowWidth, windowHeight, WEBGL);
     textSize(width / 3);
     textAlign(CENTER, CENTER);
-
     
     windowResized();
 }
@@ -36,13 +35,18 @@ function setup()
 
 function windowResized() 
 {
-    floorWidth = windowWidth - 50;
+    var smallestDimension = windowWidth;
+
+    if(windowHeight < smallestDimension){ smallestDimension = windowHeight; }
+
+    var floorWidth = smallestDimension;
 
     ONE_FOOT = floorWidth / FLOOR_SIZE;
 
     resizeCanvas(windowWidth, windowHeight);
 }
 
+// TODO: Load font to display FPS
 function drawFps()
 {
     if(SHOW_FPS)
@@ -67,10 +71,16 @@ function drawCopter()
     );
 
 
-    scale((COPTER_SCALE));
+    scale(COPTER_SCALE * (1/65) * ONE_FOOT);
     model(COPTER_MODEL);
     pop();
 }
+
+function SideView()
+{
+    console.warn("Foo");
+}
+
 
 function InterpolatePosition()
 {
@@ -109,7 +119,6 @@ function InterpolatePosition()
 
 function drawFloor()
 {
-    var newOneFoot = ONE_FOOT ;
     push();
     strokeWeight(0);
 
@@ -117,12 +126,16 @@ function drawFloor()
     translate(0,0,1);
     rectMode(RADIUS); 
     fill("#FF0000"); 
-    rect(0, 0, newOneFoot * .5, .5 * newOneFoot);
+    rect(0, 0, ONE_FOOT * .5, .5 * ONE_FOOT);
     pop();
 
     // Translate so that the copter lies in the middle of 
     // the floor.
-    translate(FLOOR_SIZE * newOneFoot * -.5 + newOneFoot * .5, FLOOR_SIZE * newOneFoot * -.5 + newOneFoot * .5)
+    translate
+    (   
+        FLOOR_SIZE * ONE_FOOT * -.5 + ONE_FOOT * .5,
+        FLOOR_SIZE * ONE_FOOT * -.5 + ONE_FOOT * .5
+    );
 
     for(var x = 0; x < FLOOR_SIZE; x++)
     {
@@ -134,20 +147,26 @@ function drawFloor()
                 fill("white");
             }else { fill("grey"); }
 
-            translate(x * newOneFoot, y * newOneFoot, 0);
-            plane(newOneFoot, newOneFoot);
+            translate(x * ONE_FOOT, y * ONE_FOOT, 0);
+            plane(ONE_FOOT, ONE_FOOT);
             pop();
         }
     }
 
-
     pop();
+}
+
+function UpdateCamera()
+{
+    camera(CAMERA_POS.x, CAMERA_POS.y, CAMERA_POS.z,
+        CAMERA_FOCUS.x, CAMERA_FOCUS.y, CAMERA_FOCUS.z,
+        0, 1, 0);
 }
 
 // Called every frame
 function draw()
 {
-    orbitControl();
+    UpdateCamera();
 
     background("#AFAFAF");
 
@@ -158,5 +177,4 @@ function draw()
     InterpolatePosition();
 
     drawCopter();
-
 }
