089dd919ab529afffaadf496436a2d951d189b07
Jeffrey M. Brown <brown.jeffreym@gmail.com>
2019-10-08 17:55:01 -0500

    updated test file for newer drone interface

--- client/test.py
+++ client/test.py
@@ -1,10 +1,12 @@
 from drone import Drone
 
 with Drone() as d:
-    d.speed()
     d.battery()
-    d.up(100)
-    d.forward(100)
-    d.back(100)
-    d.ccw(360)
-    
+    d.takeoff()
+    d.down(20)
+    d.go(500, 0, 0)
+    d.cw(180)
+    d.go(500, 0, 0)
+    d.cw(180)
+
+
