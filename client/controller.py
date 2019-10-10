f4e9585ae0612eacaee2f1e3c3995e42a8fd50ac
Jeffrey M. Brown <brown.jeffreym@gmail.com>
2019-10-09 21:48:49 -0500

    FindTarget recognizes targets and controls drone to look at them

--- client/controller.py
+++ client/controller.py
@@ -1,4 +1,6 @@
 """Module for controlling the drone through the radio control command"""
+import cv2
+import recognition
 
 class DroneController:
     """Intermediary for sending rc commands to drone. This class keeps
@@ -50,3 +52,50 @@ input parameters are an instance of the Drone class in drone.py and
             self.changed = True
 
         self.send()
+
+
+
+class FindTarget:
+    def __init__(self, controller, color_outer, color_inner):
+        self.controller = controller
+        self.color_outer = color_outer
+        self.color_inner = color_inner
+        self.no_target_frame_count = 0
+        self.contour_outer = None
+        self.contour_inner = None
+
+    def process(self, frame, draw=False):
+        # attempt to find two contours
+        contour_outer = recognition.find_target(frame, self.color_outer)
+        contour_inner = recognition.find_target(frame, self.color_inner)
+    
+        if (recognition.found_target(contour_outer) and
+            recognition.found_target(contour_inner) and
+            recognition.contained_within(contour_outer, contour_inner)):
+            # found target, so reset counter
+            self.no_target_frame_count = 0
+
+            # compute displacements and directions to move drone
+            dx, dy = recognition.centroid_displacement(contour_outer, frame)
+            cx, cy = recognition.image_center(frame)
+            yaw = recognition.calculate_direction(dx)
+            up_down = recognition.calculate_direction(dy)
+
+            # send drone directions
+            self.controller.set_yaw(yaw)
+            self.controller.set_up_down(up_down)
+            self.controller.send()
+
+            if draw:
+                # draw contours on frame
+                cv2.line(frame, (cx, cy), (cx+dx, cy+dy), (0, 0, 255), 1)
+                cv2.drawContours(frame, [contour_outer], -1, (0, 255, 0), 0)
+                cv2.drawContours(frame, [contour_inner], -1, (0, 255, 0), 0)
+                centroid = recognition.get_centroid(contour_outer)
+                cv2.circle(frame, centroid, 3, (255, 255, 255), -1)
+
+        else:
+            self.no_target_frame_count += 1
+            if self.no_target_frame_count > 10:
+                self.controller.stop()
+                no_target_frame_count = 0
--- client/test-color-recognition.py
+++ client/test-color-recognition.py
@@ -1,55 +1,22 @@
 import cv2
 import drone
 import controller
-import recognition as rec
+
+# colors to describe target
+color_outer = 'fuschia-webcam'
+color_inner = 'blue-webcam'
 
 drone = drone.FakeDrone()
 control = controller.DroneController(drone)
-
-color_outer = 'fuschia-webcam'
-color_inner = 'blue-webcam'
-no_target_frame_count = 0
+finder = controller.FindTarget(control, color_outer, color_inner)
 
 drone.battery()
 drone.takeoff()
 
 while True:
-    # get one video frame
     frame = drone.get_frame()
-
-    # attempt to find two contours
-    contour_outer = rec.find_target(frame, color_outer)
-    contour_inner = rec.find_target(frame, color_inner)
-    
-    if (rec.found_target(contour_outer) and rec.found_target(contour_inner) and
-        rec.contained_within(contour_outer, contour_inner)):
-
-        no_target_frame_count = 0
-
-        # compute displacements and directions to move drone
-        dx, dy = rec.centroid_displacement(contour_outer, frame)
-        cx, cy = rec.image_center(frame)
-        yaw = rec.calculate_direction(dx)
-        up_down = rec.calculate_direction(dy)
-
-        # send drone directions
-        control.set_yaw(yaw)
-        control.set_up_down(up_down)
-        control.send()
-
-        # draw contours on frame
-        cv2.line(frame, (cx, cy), (cx+dx, cy+dy), (0, 0, 255), 1)
-        cv2.drawContours(frame, [contour_outer], -1, (0, 255, 0), 0)
-        cv2.drawContours(frame, [contour_inner], -1, (0, 255, 0), 0)
-        centroid = rec.get_centroid(contour_outer)
-        cv2.circle(frame, centroid, 3, (255, 255, 255), -1)
+    finder.process(frame, draw=True)
         
-    else:
-        no_target_frame_count += 1
-        if no_target_frame_count > 10:
-            control.stop()
-            no_target_frame_count = 0
-
     cv2.imshow('window', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
