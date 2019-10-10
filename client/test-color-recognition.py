cfb317a74fcafc86f549d82d6d9721d266f4dcc6
Jeffrey M. Brown <brown.jeffreym@gmail.com>
2019-10-09 21:24:28 -0500

    Added two-color target detection, cleaned up code, added comments

--- client/test-color-recognition.py
+++ client/test-color-recognition.py
@@ -6,27 +6,42 @@ import recognition as rec
 drone = drone.FakeDrone()
 control = controller.DroneController(drone)
 
-color = 'fuschia-webcam'
+color_outer = 'fuschia-webcam'
+color_inner = 'blue-webcam'
 no_target_frame_count = 0
 
 drone.battery()
 drone.takeoff()
 
 while True:
+    # get one video frame
     frame = drone.get_frame()
-    contour = rec.find_target(frame, color)
-    if rec.found_target(contour):
+
+    # attempt to find two contours
+    contour_outer = rec.find_target(frame, color_outer)
+    contour_inner = rec.find_target(frame, color_inner)
+    
+    if (rec.found_target(contour_outer) and rec.found_target(contour_inner) and
+        rec.contained_within(contour_outer, contour_inner)):
+
         no_target_frame_count = 0
-        dx, dy = rec.centroid_displacement(contour, frame)
+
+        # compute displacements and directions to move drone
+        dx, dy = rec.centroid_displacement(contour_outer, frame)
         cx, cy = rec.image_center(frame)
-        cv2.line(frame, (cx, cy), (cx+dx, cy+dy), (0, 0, 255), 1)
         yaw = rec.calculate_direction(dx)
-        control.set_yaw(yaw)
         up_down = rec.calculate_direction(dy)
+
+        # send drone directions
+        control.set_yaw(yaw)
         control.set_up_down(up_down)
         control.send()
-        cv2.drawContours(frame, [contour], -1, (0, 255, 0), 0)
-        centroid = rec.get_centroid(contour)
+
+        # draw contours on frame
+        cv2.line(frame, (cx, cy), (cx+dx, cy+dy), (0, 0, 255), 1)
+        cv2.drawContours(frame, [contour_outer], -1, (0, 255, 0), 0)
+        cv2.drawContours(frame, [contour_inner], -1, (0, 255, 0), 0)
+        centroid = rec.get_centroid(contour_outer)
         cv2.circle(frame, centroid, 3, (255, 255, 255), -1)
         
     else:
