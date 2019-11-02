f65f648fbdc0ff398bf9548a411103de9c0cd8c5
Jeffrey M. Brown <brown.jeffreym@gmail.com>
2019-11-02 16:53:11 -0500

    Test script for aligning the drone using a finite state machine

--- client/test-color-recognition.py
+++ client/test-color-recognition.py
@@ -2,6 +2,7 @@ import cv2
 import drone
 import controller
 import recognition
+import fsm
 
 # colors to describe target
 color_outer = 'fuschia' # 'fuschia-webcam'
@@ -12,14 +13,18 @@ drone = drone.WebcamDrone()
 control = controller.DroneController(drone)
 identifier = recognition.TargetIdentifier(color_outer, color_inner)
 finder = recognition.FindTarget(control, identifier)
+aligner = fsm.AlignmentFSM(identifier, control)
 
 drone.battery()
 drone.takeoff()
 
 while True:
     frame = drone.get_frame()
-    finder.process(frame, draw=True)
-        
+
+    if aligner.on_frame(frame) is None:
+        # succeeded in alignment
+        break
+
     cv2.imshow('window', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
