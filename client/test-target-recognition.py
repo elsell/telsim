f1721177bef426d81a31ca21f6f6719922d4449d
Jeffrey M. Brown <brown.jeffreym@gmail.com>
2019-11-05 13:42:14 -0600

    Added script for testing target recognition

---
+++ client/test-target-recognition.py
@@ -0,0 +1,27 @@
+import cv2
+import drone
+import controller
+import recognition
+
+# colors to describe target
+color_outer = 'fuschia' # 'fuschia-webcam'
+color_inner = 'blue' # 'blue-webcam'
+identifier = recognition.TargetIdentifier(color_outer, color_inner)
+
+#drone = drone.WebcamDrone()
+drone = drone.Drone()
+
+drone.battery()
+
+while True:
+    frame = drone.get_frame()
+    identifier.find_target(frame)
+
+    cv2.imshow('window', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+
+
